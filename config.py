725521e23349728b30124c693d525d9f5f353bc9
MR.DEV <notsolo045@gmail.com>
2026-02-06 09:25:47 +0500

    Sss

--- config.py
+++ config.py
@@ -3,5 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID = int(os.getenv('ADMIN_ID'))
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7979034038:AAGs3G1CtNZjju5acfdCMMHbbVwE7yDH7Qc')
+ADMIN_ID = int(os.getenv('ADMIN_ID', '7176985245'))
+
+# Xatoliklarni tekshirish
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN topilmadi! .env faylini tekshiring.")
+
+if not ADMIN_ID:
+    raise ValueError("ADMIN_ID topilmadi! .env faylini tekshiring.")
