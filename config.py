363d16ef11edb08b0bec10cd6e2c76a692272334
DebySanjar <notsolo045@gmail.com>
2026-04-06 13:15:41 +0500

    opa aytganini togrilladimi

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ MEDICINE_ADMIN_ID = 547584005  # Dori buyurtma admin
 # Barcha adminlar ro'yxati
 ALL_ADMINS = [ADMIN_ID, SUPER_ADMIN_ID, MEDICINE_ADMIN_ID]
 
+
 # Majburiy kanal
 
 CHANNEL_ID = "@Burj_spravichniy"  # Kanal username
